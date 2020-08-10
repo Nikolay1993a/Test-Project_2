80be8545330b4311460b2a6a4097f4075a9b6341
Nikolay <satana1993a@gmail.com>
2020-08-10 20:21:57 +0300

    Modified main form by using promises

--- js/script.js
+++ js/script.js
@@ -117,37 +117,53 @@ window.addEventListener('DOMContentLoaded', function() {
 
     statusMessage.classList.add('status');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
+    function sendForm(form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+            
+            let formData = new FormData(form);
+    
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'aplication/json; charset = utf-8');
+            function postData(json) {
 
-        let formData = new FormData(form);
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    
+                    request.open('POST', 'server.php');
 
-        let obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
+                    request.setRequestHeader('Content-type', 'aplication/json; charset = utf-8');
 
-        request.send(json);
-
-        request.addEventListener('readystatechange', function() {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    };
+                    request.send(json);
+                });
             }
+            
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+            
+            postData(json)
+                    .then(() => statusMessage.innerHTML = message.loading)
+                    .then(() => statusMessage.innerHTML = message.success)
+                    .catch(() => statusMessage.innerHTML = message.failure)
+                    .then(clearInput)
         });
-
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }
-    });
-
+    }
+    sendForm(form);
 }); 
